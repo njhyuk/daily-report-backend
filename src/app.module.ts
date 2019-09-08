bfd4b257b7f75cb1a2334bbb664c2894af47c103
njhyuk <njh2239@gmail.com>
2019-09-08 16:39:31 +0900

    mogdb connect test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: '10.19.254.201',
+      port: 27017,
+      username: 'user',
+      password: 'test1234',
+      database: 'dailyReport',
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
